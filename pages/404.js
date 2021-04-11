2ccd6f8a032dfce4f9cfcada562a5cf4f95c7a43
Rıdvan Karadağ <ridvan.karadag@softtech.com.tr>
2021-04-11 19:41:18 +0300

    feat(404): add custom 404

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+export default function Custom404() {
+    return <div className="mx-auto w-6/12 h-screen flex flex-col items-center justify-center space-y-6">
+        <h1>Sitemiz yapım aşamasında. Tüm güncellemelerden haberdar olmak ve erken erişim fırsatlarından yararlanmak için ön başvuru sürecini tamamlayınız.</h1>
+        <a href="https://forms.gle/JNhzw1ssRi2krKpm7" className="bg-indigo-900 text-white font-semibold px-8 py-2 rounded-md shadow-lg hover:bg-indigo-800 transition">Başvur</a>
+    </div>
+}
